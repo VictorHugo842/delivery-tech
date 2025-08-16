2d7f6fb9df9a7e478fe190758e9e1f747d501cf9
VictorHugo842 <victor99.santos@gmail.com>
2025-08-16 11:16:11 -0300

    add DTO ClienteRequest,ClienteResponse, Cliente model, dependency, repository, service, service impl

---
+++ src/main/java/com/deliverytech/delivery_api/repository/ClienteRepository.java
@@ -0,0 +1,11 @@
+package com.deliverytech.delivery_api.repository;
+
+import com.deliverytech.delivery_api.model.Cliente;
+import org.springframework.data.jpa.repository.*;
+
+
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+    boolean existsByEmail(String email);
+}
+
+
